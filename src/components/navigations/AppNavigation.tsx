307b1f269f4a0af347414b35a00c1cc5f2ae793e
BeastEren <souravsinghami1@gmail.com>
2024-08-06 20:41:25 +0530

    ...

--- src/components/navigations/AppNavigation.tsx
+++ src/components/navigations/AppNavigation.tsx
@@ -1,15 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
 
-// const Stack = createNativeStackNavigator()
+// // const Stack = createNativeStackNavigator()
 
-export default function AppNavigation() {
-    return (
-        <View>
-            <Text>AppNavigation</Text>
-        </View>
-    )
-}
+// export default function AppNavigation() {
+//     return (
+//         <View>
+//             <Text>AppNavigation</Text>
+//         </View>
+//     )
+// }
 
-const styles = StyleSheet.create({})
-
+// const styles = StyleSheet.create({})
